a062000139021aac582ba91f4510a2f6e3b320bf
Paul Saxe <paul@thesaxes.net>
2020-12-15 09:35:41 -0500

    Updating the documentation build system to be more user-friendly

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -1,143 +1,164 @@
 #!/bin/bash
-set -x
+# set -x
 ################################################################################
 # File:    buildDocs.sh
 # Purpose: Script that builds our documentation using sphinx and updates GitHub
 #          Pages. This script is executed by:
-#            .github/workflows/docs_pages_workflow.yml
+#            .github/workflows/*.yml
 #
 # Authors: Michael Altfield <michael@michaelaltfield.net>
+#          Paul Saxe <psaxe@vt.edu>
 # Created: 2020-07-17
-# Updated: 2020-07-20
-# Version: 0.2
+# Updated: 2020-12-12
+# Version: 0.3
 ################################################################################
  
 #####################
 # DECLARE VARIABLES #
 #####################
  
+echo "::group::setup"
 pwd
 ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
  
 # make a new temp dir which will be our GitHub Pages docroot
 docroot=`mktemp -d`
+mkdir "${docroot}/dev"
 
 export REPO_NAME="${GITHUB_REPOSITORY##*/}"
+echo "::endgroup::"
  
 ##############
 # BUILD DOCS #
 ##############
- 
-# first, cleanup any old builds' static assets
-make -C docs clean
- 
+
+echo "::group::branches"
 # get a list of branches, excluding 'HEAD' and 'gh-pages'
 versions="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/remotes/origin/ | grep -viE '^(HEAD|gh-pages)$'`"
-for current_version in ${versions}; do
- 
-   # make the current language available to conf.py
-   export current_version
-   git checkout --no-guess ${current_version}
- 
-   echo "INFO: Building sites for ${current_version}"
- 
-   # skip this branch if it doesn't have our docs dir & sphinx config
-   if [ ! -e 'docs/conf.py' ]; then
-      echo -e "\tINFO: Couldn't find 'docs/conf.py' (skipped)"
-      continue
-   fi
- 
-   languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
-   for current_language in ${languages}; do
- 
-      # make the current language available to conf.py
-      export current_language
- 
-      ##########
-      # BUILDS #
-      ##########
-      echo "INFO: Building for ${current_language}"
- 
-      # HTML #
-      sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
- 
-      # # PDF #
-      # sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
-      # mkdir -p "${docroot}/${current_language}/${current_version}"
-      # cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.pdf"
- 
-      # # EPUB #
-      # sphinx-build -b epub docs/ docs/_build/epub -D language="${current_language}"
-      # mkdir -p "${docroot}/${current_language}/${current_version}"
-      # cp "docs/_build/epub/target.epub" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.epub"
- 
-      # copy the static assets produced by the above build into our docroot
-      rsync -av "docs/_build/html/" "${docroot}/"
- 
-   done
- 
+echo "Branches"
+for current_version in ${versions}
+do
+    echo "       ${current_version}"
 done
+echo "::endgroup::"
+
+for current_version in ${versions}
+do
+    echo "::group::build ${current_version}"
  
-# return to main branch
-git checkout main
+    # make the current language available to conf.py
+    export current_version
+    git checkout ${current_version}
+ 
+    # skip this branch if it doesn't have our docs dir & sphinx config
+    if [ ! -e 'docs/conf.py' ]; then
+	echo "::warning::Skipping because could not find 'docs/conf.py'"
+	continue
+    fi
+ 
+    languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
+    for current_language in ${languages}
+    do
+	# make the current language available to conf.py
+	export current_language
+	
+	##########
+	# BUILDS #
+	##########
+	echo "Building for ${current_language}"
+
+	# first, cleanup any old builds' static assets
+	make -C docs clean
+	
+	# HTML #
+	sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
+	
+	# # PDF #
+	# sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
+	# mkdir -p "${docroot}/${current_language}/${current_version}"
+	# cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.pdf"
+	
+	# # EPUB #
+	# sphinx-build -b epub docs/ docs/_build/epub -D language="${current_language}"
+	# mkdir -p "${docroot}/${current_language}/${current_version}"
+	# cp "docs/_build/epub/target.epub" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.epub"
+	
+	# copy the static assets produced by the above build into our docroot
+	if [ "${current_version}" = "main" -a "${current_language}" = "en" ]
+	then
+	    echo "Publishing main to /"
+	    rsync -a "docs/_build/html/en/main/" "${docroot}/"
+	else
+	    echo "Publishing ${current_version} to /dev/${current_language}/${current_version}/"
+	    rsync -a "docs/_build/html/" "${docroot}/dev/"
+	fi
+    done
+    echo "::endgroup::"
+done
  
 #######################
 # Update GitHub Pages #
 #######################
+echo "::group::create index files, etc."
+ 
+# return to main branch
+git checkout main
  
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-
  
 # add redirect from the docroot to our default docs language/version
-cat > "${docroot}/index.html" <<EOF
+cat > "${docroot}/dev/index.html" <<EOF
 <!DOCTYPE html>
 <html>
    <head>
-      <title>System Step Documentation</title>
+      <title>Documentation for the System plug-in for SEAMM</title>
    </head>
    <body>
-      <h1>System Step Documentation</h1>
+      <h1>Documentation for the System plug-in for SEAMM</h1>
       <h2>Branches</h2>
       <ul>
 EOF
 
-for current_version in ${versions}; do
-   git checkout --no-guess ${current_version}
-
-   # skip this branch if it doesn't have our docs dir & sphinx config
-   if [ ! -e 'docs/conf.py' ]; then
-      echo -e "\tINFO: Couldn't find 'docs/conf.py' (skipped)"
-      continue
-   fi
-   cat >> "${docroot}/index.html" <<EOF
-        <li><a href="/${REPO_NAME}/en/${current_version}/">${current_version}</a></li>
+for current_version in ${versions}
+do
+    if [ "${current_version}" = "main" ]
+    then
+	cat >> "${docroot}/dev/index.html" <<EOF
+        <li><a href="../">Stable version (main)</a></li>
 EOF
+    fi
 done
 
-cat >> "${docroot}/index.html" <<EOF
+for current_version in ${versions}
+do
+    git checkout --no-guess ${current_version}
+
+    # skip this branch if it doesn't have our docs dir & sphinx config
+    if [ ! -e 'docs/conf.py' ]; then
+	continue
+    fi
+    if [ "${current_version}" != "main" ]
+    then
+	cat >> "${docroot}/dev/index.html" <<EOF
+        <li><a href="en/${current_version}/">${current_version}</a></li>
+EOF
+    fi
+done
+
+cat >> "${docroot}/dev/index.html" <<EOF
       </ul>
    </body>
 </html>
 EOF
-
-# Now go to the directory...
-pushd "${docroot}"
-
-# ls -laR .
-
-# don't bother maintaining history; just generate fresh
-git init
-git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git checkout -b gh-pages
  
 # add .nojekyll to the root so that github won't 404 on content added to dirs
 # that start with an underscore (_), such as our "_content" dir..
-touch .nojekyll
+touch ${docroot}/.nojekyll
  
 # Add README
-cat > README.md <<EOF
+cat > ${docroot}/README.md <<EOF
 # GitHub Pages Cache
  
 Nothing to see here. The contents of this branch are essentially a cache that's not intended to be viewed on github.com.
@@ -149,6 +170,17 @@ For more information on how this documentation is built using Sphinx, Read the D
  
  * https://tech.michaelaltfield.net/2020/07/18/sphinx-rtd-github-pages-1
 EOF
+
+echo "::endgroup::"
+echo "::group::push to gh-pages"
+
+# Now go to the directory...
+pushd "${docroot}"
+
+# don't bother maintaining history; just generate fresh
+git init
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git checkout -b gh-pages
  
 # copy the resulting html pages built from sphinx above to our new git repo
 git add .
@@ -159,8 +191,8 @@ git commit -am "${msg}"
  
 # overwrite the contents of the gh-pages branch on our github.com repo
 git push deploy gh-pages --force
- 
+
+echo "::endgroup::" 
+
 popd # return to main repo sandbox root
- 
-# exit cleanly
-# exit 0
+echo "All done! Documentation built successfully."
