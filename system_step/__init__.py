44b52c930c8daf92c2981867c6c19c12c653d9ec
Paul Saxe <paul@thesaxes.net>
2021-03-15 02:37:38 -0400

    Fixed yapf error.

--- system_step/__init__.py
+++ system_step/__init__.py
@@ -13,8 +13,12 @@ from system_step.system_parameters import SystemParameters  # noqa: F401, E501
 from system_step.system_step import SystemStep  # noqa: F401, E501
 from system_step.tk_system import TkSystem  # noqa: F401, E501
 
+# The metadata
+from system_step.metadata import properties  # noqa: F401
+
 # Handle versioneer
 from ._version import get_versions
+
 __author__ = """Paul Saxe"""
 __email__ = 'psaxe@molssi.org'
 versions = get_versions()
